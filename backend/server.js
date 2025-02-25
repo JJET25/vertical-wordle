415a8fb3945b16ef6f42178889adf27f02a70360
Jesus Espinoza Torruco <jespinoza2511@hotmail.com>
2025-02-24 18:20:04 -0600

    commit message

--- backend/server.js
+++ backend/server.js
@@ -3,18 +3,31 @@ import cors from 'cors';
 import wordController from './controllers/word.controller.js';
 import { connectDB } from './config/db.js';
 import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 dotenv.config();
-
 connectDB();
 
 const app = express();
 const PORT = process.env.PORT || 5005;
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+console.log("Server directory:", __dirname);
+console.log("Serving static files from:", path.join(__dirname, '../client/dist'));
+
 app.use(cors());
 app.use(express.json());
 
+app.use(express.static(path.join(__dirname, '../client/dist')));
+
 app.use('/api/words', wordController);
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
